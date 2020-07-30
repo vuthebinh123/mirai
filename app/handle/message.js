88d62d0269251b9783afd40e8af5a3d4de0c7be5
Catalizcs <roxtigger2003@users.noreply.github.com>
2020-07-30 23:21:00 +0700

    fast fix#2

--- app/handle/message.js
+++ app/handle/message.js
@@ -477,7 +477,7 @@ module.exports = function({ api, modules, config, __GLOBAL, User, Thread, Rank, 
 				var callback = () => api.sendMessage({attachment: fs.createReadStream(__dirname + "/src/video.mp4")}, threadID, () => fs.unlinkSync(__dirname + "/src/video.mp4"));
 				if (content.indexOf("http") == -1) content = (await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&key=${googleSearch}&q=${encodeURIComponent(content)}`, {responseType: 'json'})).data.items[0].id.videoId;
 				ytdl.getInfo(content, (err, info) => if (info.length_seconds > 360) return api.sendMessage("Độ dài video vượt quá mức cho phép, tối đa là 6 phút!", threadID, messageID)});
-				return ytdl(text).pipe(fs.createWriteStream(__dirname + "/src/video.mp4")).on("close", () => callback());
+				return ytdl(content).pipe(fs.createWriteStream(__dirname + "/src/video.mp4")).on("close", () => callback());
 			})();
 
 		//anime
